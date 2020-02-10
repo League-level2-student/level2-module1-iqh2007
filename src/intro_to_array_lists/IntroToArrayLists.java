f03ae163c25e30cb631977fbf3a92f1fb65f6ab3
League Student <league@WTS14-3.attlocal.net>
2020-02-09 17:25:42 -0800

    GuestBook, IntroToArrayLists

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,54 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
+
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> strings = new ArrayList<String>();
+
+		// 2. Add five Strings to your list
+		String a = " red ";
+		String b = " orange ";
+		String c = " yellow ";
+		String d = " green ";
+		String e = " blue ";
+
+		strings.add(a);
+		strings.add(b);
+		strings.add(c);
+		strings.add(d);
+		strings.add(e);
+
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < strings.size(); i++) {
+
+			String j = strings.get(i);
+			System.out.println(j);
+
+		}
+
+		// 4. Print all the Strings using a for-each loop
+		for (String s : strings) {
+			System.out.println(s);
+		}
+
+		// 5. Print only the even numbered elements in the list.
+		for(int i = 0; i <strings.size(); i++) {
+			if(i%)
+		}
 		
-		//2. Add five Strings to your list
 		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+
+		// 6. Print all the Strings in reverse order.
+		for (int i = strings.size()-1; i >= 0; i--) {
+
+			String j = strings.get(i);
+			System.out.println(j);
+		}
+
+		// 7. Print only the Strings that have the letter 'e' in them.
 	}
 }
